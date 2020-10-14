eaed4da7150132bc89c0df73cc469daa0bbb004b
abdo28 <abedalkhaleq28@gmail.com>
2020-10-14 13:13:30 +0300

    implemetn Max class and test

--- src/main/java/ListMaxService.java
+++ src/main/java/ListMaxService.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class ListMaxService {
+
+    private ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    public ListMaxService(ArrayList<Integer> arr){
+        this.arr = arr;
+    }
+
     public int getMax(){
-        throw new RuntimeException();
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i)>max){
+                max=arr.get(i);
+            }
+        }
+        return max;
     }
 }
 
--- src/test/java/ListMaxServiceTest.java
+++ src/test/java/ListMaxServiceTest.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ListMaxServiceTest {
+
+    @Test
+    public void testGetMax(){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+
+        ListMaxService testService = new ListMaxService(arr);
+        Assert.assertEquals(3, testService.getMax());
+    }
 }
